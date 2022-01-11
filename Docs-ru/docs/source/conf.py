13c73209c797cc5182a26c5d91733736a8e06264
TexnoIThttps://texnoit.com/ <56906486+Texnoit-com@users.noreply.github.com>
2022-01-12 00:17:28 +0500

    installation, configuration theme:sphinx-rtd-theme

--- Docs-ru/docs/source/conf.py
+++ Docs-ru/docs/source/conf.py
@@ -1,7 +1,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Microbit-ru'
-copyright = '2022, Maxim Bekurin'
+copyright = '2022, Maxim Bekurin, Read the Docs, Inc. & contributors'
 author = 'Maxim Bekurin'
 
 # The full version, including alpha/beta/rc tags
@@ -22,6 +22,22 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': 'view',
+    'style_nav_header_background': '#2980B9',
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 html_static_path = ['_static']
