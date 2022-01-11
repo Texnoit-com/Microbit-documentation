d4d831f65ae0e15208018510f347e9272bfdb083
TexnoIThttps://texnoit.com/ <56906486+Texnoit-com@users.noreply.github.com>
2022-01-11 23:55:45 +0500

    extension with Russian documentation

---
+++ Docs-ru/docs/source/conf.py
@@ -0,0 +1,27 @@
+# -- Project information -----------------------------------------------------
+
+project = 'Microbit-ru'
+copyright = '2022, Maxim Bekurin'
+author = 'Maxim Bekurin'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+language = 'ru'
+
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
