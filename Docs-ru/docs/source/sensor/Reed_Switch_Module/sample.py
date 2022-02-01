817a0ca61f96f55700e32f8a8b4c784856af6569
Maxim Bekurin <BekurinMD@yandex.ru>
2022-02-01 17:42:44 +0500

    Reed_Switch

--- Docs-ru/docs/source/sensor/Reed_Switch_Module/sample.py
+++ Docs-ru/docs/source/sensor/Reed_Switch_Module/sample.py
@@ -1,5 +1,9 @@
 from microbit import *
+from Reed_Switch import Reed_Switch
 
-while  True :
-     display.scroll('Test')
-     sleep ( 1000 )
+switch=Reed_Switch(pin0)
+
+while True:
+    if switch.is_pressed():
+        display.show(Image.YES)
+        sleep(1000)
